e2d2f69a3f24a411f5c409532f005d53326421f2
Henrique R. Firmino <henrique_rfirmino@hotmail.com>
2022-01-10 12:59:11 -0400

    Update environment.ts

--- ToDo-UI/ewave-livraria-senior/src/environments/environment.ts
+++ ToDo-UI/ewave-livraria-senior/src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  todo_api: 'http://localhost:64978',
+  todo_api: 'https://livaria-todo-api.azurewebsites.net/',
   tamanho_pagina_padrao: 12,
   tamanho_pagina_reduzida: 6,
   endPointImagem: 'http://localhost:64978/api/imagens?guid='
